926b8f51f0b81ee1314ce5dde39222865197dda6
Cameron Holman cameron.holman@berkeley.edu <cameron.holman@rhino2.rhino.psych.upenn.edu>
2022-09-20 16:39:44 -0400

    update subset data

--- neuro/utils/subset_data.py
+++ neuro/utils/subset_data.py
@@ -1,23 +1,28 @@
 import numpy as np
 
 
-def subset_epoch_events(event_times, epoch_start_times, epoch_end_times):
+def subset_epoch_events(event_times, epoch_start_times, epoch_stop_times):
     """ Given 1D array of event times, subset only those times in epochs of interest
 
     ex. all spikes within trial epochs, all position times in navigation epoch
     """
+    # Convert float/int times to list 
+    if type(epoch_start_times) in [float, int] or type(epoch_stop_times) in [float, int]:
+        epoch_start_times = [epoch_start_times]
+        epoch_stop_times  = [epoch_stop_times]
+    
+    # Main
     EE = np.array([])
     for ix in range(len(epoch_start_times)):
-        EE = np.append(
-            EE,
+        EE = np.append(EE,
             event_times[
                 (event_times > epoch_start_times[ix]) & \
-                (event_times < epoch_end_times[ix])
+                (event_times < epoch_stop_times[ix])
                 ],   
             axis = 0)
     return EE
 
-def subset_epoch_data(data, event_times, epoch_start_times, epoch_end_times):
+def subset_epoch_data(data, event_times, epoch_start_times, epoch_stop_times):
     """ 
     Given 1D or 2D array of data (ex 1D head direction degrees, 2D position times) 
     and 1D array of matched event_times (ex times at which HD is recorded) with 
@@ -25,10 +30,15 @@ def subset_epoch_data(data, event_times, epoch_start_times, epoch_end_times):
 
     Pull out data that falls in epochs of interes (ex navigation epochs)
     """
+    # Convert float/int times to list 
+    if type(epoch_start_times) in [float, int] or type(epoch_stop_times) in [float, int]:
+        epoch_start_times = [epoch_start_times]
+        epoch_stop_times  = [epoch_stop_times]
+
+    # Main
     epoch_ixs = np.array([], dtype=int)
     for ix in range(len(epoch_start_times)):
-        epoch_ixs_ix = np.where((epoch_start_times[ix] < event_times) \
-                                        & (event_times < epoch_end_times[ix]))
+        epoch_ixs_ix = np.where((epoch_start_times[ix] < event_times) & (event_times < epoch_stop_times[ix]))
         epoch_ixs = np.append(epoch_ixs, epoch_ixs_ix[0])
     
     if data.ndim == 1:
